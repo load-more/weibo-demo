2775e05f60d45d44e74b3d019ba29815ea47a5d5
Shallowbeing <load_more@163.com>
2021-06-02 22:45:53 +0800

    feature: 添加注册功能

--- src/controller/user.js
+++ src/controller/user.js
@@ -3,21 +3,50 @@
  * @author load_more
  */
 
-const { getUserInfo } = require('../service/user')
 const { SuccessModel, ErrorModel } = require('../model/ResModel')
-const { registerUsernameExistInfo } = require('../model/ErrorInfo')
+const {
+  getUserInfo,
+  createUser
+} = require('../service/user')
+const {
+  registerUsernameExistInfo,
+  registerUsernameNotExistInfo,
+  registerFailedInfo
+} = require('../model/ErrorInfo')
 
 /**
+ * @description 判断用户名是否存在
  * @param {string} username 用户名
  */
 async function isExist(username) {
   const userInfo = await getUserInfo(username)
   if (userInfo) { // 如果存在userInfo
+    return new SuccessModel(userInfo)
+  }
+  return new ErrorModel(registerUsernameNotExistInfo)
+}
+
+
+/**
+ * @description 注册
+ * @param {*} { username, password, gender }
+ * @return {*} 
+ */
+async function register({ username, password, gender }) {
+  const userInfo = await getUserInfo(username)
+  if (userInfo) { // 用户名已存在
     return new ErrorModel(registerUsernameExistInfo)
   }
-  return new SuccessModel(userInfo)
+  try {
+    const rst = await createUser({ username, password, gender })
+    return new SuccessModel(rst)
+  } catch (ex) {
+    console.error(ex)
+    return new ErrorModel(registerFailedInfo)
+  }
 }
 
 module.exports = {
   isExist,
+  register
 }
--- src/model/ErrorInfo.js
+++ src/model/ErrorInfo.js
@@ -2,5 +2,13 @@ module.exports = {
   registerUsernameExistInfo: {
     errno: 10001,
     message: '用户名已存在'
-  }
+  },
+  registerUsernameNotExistInfo: {
+    errno: 10002,
+    message: '用户名未存在'
+  },
+  registerFailedInfo: {
+    errno: 10003,
+    message: '注册失败'
+  },
 }
