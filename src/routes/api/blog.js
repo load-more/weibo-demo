9eb417ae9e56916082e27b496125f21c41a6011f
Shallowbeing <load_more@163.com>
2021-06-20 17:23:18 +0800

    feat: 登录验证，格式校验

--- src/routes/api/blog.js
+++ src/routes/api/blog.js
@@ -1,10 +1,13 @@
 const router = require('koa-router')()
 const { createBlog } = require('../../controller/blog')
+const loginCheck = require('../../middleware/loginCheck')
+const { genValidator } = require('../../middleware/validator')
+const validateBlog = require('../../validator/blog')
 
 
 router. prefix('/api/blogs')
 
-router.post('/create', async (ctx, next) => {
+router.post('/create', genValidator(validateBlog), loginCheck, async (ctx, next) => {
   const { content, image } = ctx.request.body
   ctx.body = await createBlog(ctx, content, image)
 })
