6fce4d88702348a0d5f93a922649f42d6c8d5ef6
Shallowbeing <load_more@163.com>
2021-06-22 20:31:16 +0800

    feat: 添加单元测试

--- test/blog/blog.test.js
+++ test/blog/blog.test.js
@@ -1,6 +1,7 @@
 const server = require('../server')
 
-const COOKIE = 'koa.sid=VFlyql4iyKjcOertNLHqWsQntADmbraU; koa.sid.sig=Gj7Q1ZEg2FiisIja3edv-RbCeaw; sessionId=_1624284752756'
+// 测试用户的cookie，需要更新
+const COOKIE = 'koa.sid=DP6yV5lZxnlfdYHMbqSMQEQiKgxzjoIQ; koa.sid.sig=Gie13wYL7WOOz-1b3HJ1hgx6WY8; sessionId=_1624364980914'
 
 // 创建博客
 test('创建博客，应该成功', async () => {
@@ -28,6 +29,13 @@ test('获取自己的博客，应该成功', async () => {
     .get('/api/blogs/home/0')
     .set('cookie', COOKIE)
   expect(res.body.errno).toBe(0)
+
+  const data = res.body.data
+  expect(data).toHaveProperty('isEmpty')
+  expect(data).toHaveProperty('blogList')
+  expect(data).toHaveProperty('pageSize')
+  expect(data).toHaveProperty('pageIndex')
+  expect(data).toHaveProperty('count')
 })
 
 // 获取他人博客
@@ -37,4 +45,26 @@ test('获取他人的博客，应该成功', async () => {
     .get('/api/blogs/profile/zhangsan/0')
     .set('cookie', COOKIE)
   expect(res.body.errno).toBe(0)
+
+  const data = res.body.data
+  expect(data).toHaveProperty('isEmpty')
+  expect(data).toHaveProperty('blogList')
+  expect(data).toHaveProperty('pageSize')
+  expect(data).toHaveProperty('pageIndex')
+  expect(data).toHaveProperty('count')
+})
+
+// 获取广场页博客
+test('获取广场页博客，应该成功', async () => {
+  // 开始测试
+  const res = await server
+    .get('/api/blogs/square/0')
+  expect(res.body.errno).toBe(0)
+
+  const data = res.body.data
+  expect(data).toHaveProperty('isEmpty')
+  expect(data).toHaveProperty('blogList')
+  expect(data).toHaveProperty('pageSize')
+  expect(data).toHaveProperty('pageIndex')
+  expect(data).toHaveProperty('count')
 })
